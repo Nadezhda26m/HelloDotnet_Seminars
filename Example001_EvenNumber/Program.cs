50bc1d1f3d0b29ab5f993a09b9b60ff7bfc8679b
Nadezhda26m <nadezhda-26m@mail.ru>
2022-09-23 11:57:39 +0300

    Example 001 Even Number Random

---
+++ Example001_EvenNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число 
+// и выдаёт, является ли число чётным 
+// (делится ли оно на два без остатка).
+
+Console.Clear();
+int number = new Random().Next(-100, 101);
+Console.Write("Задумано число ");
+Console.WriteLine(number);
+if (number % 2 == 0)
+{
+    Console.WriteLine("Число является четным");
+}
+else
+{
+    Console.WriteLine("Число не является четным");
+}
