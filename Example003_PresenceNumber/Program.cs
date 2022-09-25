bd36af9d40d1f79d74ad8e167fd2b50f76f12264
Nadezhda26m <nadezhda-26m@mail.ru>
2022-09-25 17:22:10 +0300

    Example 003 Presence Number Ver1

---
+++ Example003_PresenceNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿// Определить, присутствует ли в заданном 
+// массиве некоторое число.
+// Сделаем уточнение
+// 1. Массив задается автоматически
+// 2. Искомое число загадывается компьютером
+
+Console.Clear();
+int count = 10;
+int[] array = new int[count];
+int find = new Random().Next(1, 21);
+int index = 0; // объявление переменной и инициализация
+
+while (index < count)
+{
+    array[index] = new Random().Next(1, 101);
+    index++;
+}
+
+index = 0; // обнуление объявленной выше переменной (без int)
+while (index < count)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine($"Число {find} найдено");
+    }
+    index++;
+}
+
+Console.WriteLine("End");
