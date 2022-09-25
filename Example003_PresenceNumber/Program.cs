98ffa141c49f8e7506696286d18bb8691695ac04
Nadezhda26m <nadezhda-26m@mail.ru>
2022-09-25 17:47:18 +0300

    Example 003 add User Interface

--- Example003_PresenceNumber/Program.cs
+++ Example003_PresenceNumber/Program.cs
@@ -9,21 +9,24 @@ int count = 10;
 int[] array = new int[count];
 int find = new Random().Next(1, 21);
 int index = 0; // объявление переменной и инициализация
+Console.WriteLine($"Ищем число {find}");
+Console.Write("Массив: ");
 
 while (index < count)
 {
     array[index] = new Random().Next(1, 101);
+    Console.Write(array[index] + " ");
     index++;
 }
-
+// Console.WriteLine();
 index = 0; // обнуление объявленной выше переменной (без int)
 while (index < count)
 {
     if (array[index] == find)
     {
-        Console.WriteLine($"Число {find} найдено");
+        Console.WriteLine($"\nЧисло {find} найдено"); // \n добавляет переход на новую строку перед текстом
     }
     index++;
 }
 
-Console.WriteLine("End");
+Console.WriteLine("\nEnd"); // всегда с новой строки
